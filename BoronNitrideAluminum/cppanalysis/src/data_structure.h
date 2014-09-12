1b77a0d84d2755c75aa7a1727e3bbd2cecc95c0f
Kyle Mills <kyle.mills@uoit.net>
2014-09-11 23:05:19 -0400

    updates to data structures

--- BoronNitrideAluminum/cppanalysis/src/data_structure.h
+++ BoronNitrideAluminum/cppanalysis/src/data_structure.h
@@ -3,6 +3,16 @@
 
 #include <string>
 #include <vector>
+using namespace std;
+
+struct atomType {
+   int atomspertype;
+   string element;
+   double mass;
+   double valence;
+   string pseudopotential;
+};
+
 
 
 struct FileInfo {
@@ -10,10 +20,14 @@ struct FileInfo {
    std::string input_filename;
    std::string output_data_location;
    //File data structures
-   std::vector<string> atom_types;
+   int numatoms;
+   int numtypes;
    std::vector<int> atom_count;
 
+   std::vector<atomType> atom_types;
+
    FileInfo() {
+      numatoms=0;
       input_filename = "/tmp/garbage";
       output_data_location = "/tmp/";
    }
@@ -22,17 +36,8 @@ struct FileInfo {
 
 
 
-#endif
-
-
-
-
-
-
-
-
-
-
 
 
 #endif
+
+
