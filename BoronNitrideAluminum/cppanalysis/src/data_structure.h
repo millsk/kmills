b8742f11f72ca40502e1f97daf7f8446ef27044d
Kyle Mills <kyle.mills@uoit.net>
2014-09-12 22:11:20 -0400

    added structure elements

--- BoronNitrideAluminum/cppanalysis/src/data_structure.h
+++ BoronNitrideAluminum/cppanalysis/src/data_structure.h
@@ -11,7 +11,10 @@ struct atomType {
    double mass;
    double valence;
    string pseudopotential;
-};
+   atomType () {
+      atomspertype=0; element="X  ";mass = 0.00; valence = 0.00; pseudopotential="garbage";
+   }
+}; 
 
 
 
@@ -23,13 +26,26 @@ struct FileInfo {
    int numatoms;
    int numtypes;
    std::vector<int> atom_count;
-
+   vector<double> latt_x;
+   vector<double> latt_y;
+   vector<double> latt_z;
    std::vector<atomType> atom_types;
 
    FileInfo() {
       numatoms=0;
       input_filename = "/tmp/garbage";
       output_data_location = "/tmp/";
+ 
+      latt_x.push_back(0);
+      latt_x.push_back(0);
+      latt_x.push_back(0);
+      latt_y.push_back(0);
+      latt_y.push_back(0);
+      latt_y.push_back(0);
+      latt_z.push_back(0);
+      latt_z.push_back(0);
+      latt_z.push_back(0);
+
    }
 };
 
