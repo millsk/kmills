a42ee2ef9b131f281adefa19365e3c61291942aa
Kyle Mills <kyle.mills@uoit.net>
2014-09-15 07:22:08 -0400

    comment in repairXML

--- BoronNitrideAluminum/cppanalysis/src/repairXML.sh
+++ BoronNitrideAluminum/cppanalysis/src/repairXML.sh
@@ -2,7 +2,7 @@
 
 
 #A hackish way to fix the incomplete XML from early-terminated VASP runs.  More elegant solution required
-
+#This basically just closes all dangling XML tags
 
 for tag in `xmllint $1 2>&1 > /dev/null | grep 'Premature end of data in tag' | sed 's|.*Premature end of data in tag ||g' | sed 's| line [0-9]*||g'`; do
 
