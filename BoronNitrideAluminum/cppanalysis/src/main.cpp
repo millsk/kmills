3acb7c17981ed203f563dd8ae25dc7ef0639df0a
Kyle Mills <kyle.mills@uoit.net>
2014-09-19 16:40:09 -0400

    call to MSD added

--- BoronNitrideAluminum/cppanalysis/src/main.cpp
+++ BoronNitrideAluminum/cppanalysis/src/main.cpp
@@ -2,6 +2,7 @@
 #include "read_config.cpp"
 #include "utility_functions.h"
 
+#include "msd.h"
 
 
 using namespace std;
@@ -9,16 +10,16 @@ using namespace std;
 int main() {
    cout << "\n Starting XML read"<<endl;
    FileInfo v;
-   v.input_filename="vasprun.xml";
    Configuration config;
    read_configfile(config);
-   readXML(&v);
    cout << "\n done"<<endl;
 
+   v.input_filename="vasprun.xml";
+   readXML(&v);
 
 
    if (config.msd) {
-      cout << "MSD requested for " << config.msd_atoms.size() << " atom types: " << vec2str(config.msd_atoms) << endl;;
+      msd(&v, &config);
    }
 
 
