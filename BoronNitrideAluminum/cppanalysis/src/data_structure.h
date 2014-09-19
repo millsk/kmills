a330ce6f7faa90701287ec63dbbc78b15972f7f8
Kyle Mills <kyle.mills@uoit.net>
2014-09-19 16:39:56 -0400

    unwrapping routine started, nowhere near complete

--- BoronNitrideAluminum/cppanalysis/src/data_structure.h
+++ BoronNitrideAluminum/cppanalysis/src/data_structure.h
@@ -4,15 +4,16 @@
 #include <string>
 #include <vector>
 #include <string.h>
+#include <cmath>
 using namespace std;
 
 typedef vector<float> threevector;
 
 struct TimeStep {
    vector<threevector> ppp;
+   vector<threevector> ppp_uw; //unwrapped
    vector<threevector> fff;
 };
-
 
 struct atomType {
    int atomspertype, sindex, eindex;
@@ -31,8 +32,10 @@ struct FileInfo {
    string input_filename,output_data_location,system_name;
    //File data structures
    int numatoms,numtypes,ntimesteps;
+   bool unwrapped_already=false;
    vector<int> atom_count;
-   vector<double> latt_x,latt_y,latt_z;
+//   vector<double> latt_x,latt_y,latt_z;
+   vector<threevector> latt;
    vector<atomType> atoms;
    vector<TimeStep> timesteps;
    double dt,starting_temperature; //timestep length, delta t
@@ -68,13 +71,36 @@ struct FileInfo {
       exit(0);
    }
 
+   int unwrap() {
+      if (unwrapped_already) {return 0;}
+      for (unsigned i=0; i<atoms.size(); i++) {  // for each atom type
+         cout << "Unwrapping "<< atoms[i].element << " coordinates.\n";
+         for (unsigned t=1; t < ntimesteps-1; t++) { //for each timestep
+//            cout << "t=" <<  t << "\n";
+            vector<threevector> &x0 = atoms[i].timesteps[t-1].ppp;
+            vector<threevector> &x1 = atoms[i].timesteps[t].ppp;
+            for (unsigned a=0; a<x0.size(); a++) { //for atom in ppp vector
+//               cout << "a=" << a << "\n";
+               for (int x=0; x<3; x++) {  //for each dimension (0=x,1=y,2=z) {
+//                  cout << "!!!" << x0[a][x] << "\n";
+                  if ((abs(x0[a][x] -x1[a][x])) > 0.5 ){//sqrt(pow(latt[x][0],2) + pow(latt[x][1],2) + pow(latt[x][2],2)) ) {
+                     cout << "0" ;
+                  }
+               }
+            }
+         }
+      }
+      unwrapped_already=true;
+      return 0;
+   }
+
 
    FileInfo() {
       numatoms=0;
       input_filename = "/tmp/garbage";
       output_data_location = "/tmp/";
  
-      latt_x.push_back(0);
+      /*latt_x.push_back(0);
       latt_x.push_back(0);
       latt_x.push_back(0);
       latt_y.push_back(0);
@@ -82,7 +108,7 @@ struct FileInfo {
       latt_y.push_back(0);
       latt_z.push_back(0);
       latt_z.push_back(0);
-      latt_z.push_back(0);
+      latt_z.push_back(0);*/
 
    }
 };
