0f0e720d636fe6623caee3b573d57fbe42c1368c
Kyle Mills <kyle.mills@uoit.net>
2014-09-18 22:28:42 -0400

    main.cpp is now the main compiled code. readXML.cpp is now an included file, not the main code

---
+++ BoronNitrideAluminum/cppanalysis/src/main.cpp
@@ -0,0 +1,18 @@
+#include "readXML.cpp"
+
+
+using namespace std;
+
+
+
+
+int main() {
+   cout << "\n Starting XML read"<<endl;
+   FileInfo v;
+   v.input_filename="vasprun.xml";
+   readXML(&v);
+   cout << "\n done"<<endl;
+}
+
+
+
