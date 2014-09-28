262e7c2d763f94fcbb7bc67cbdf33f3bf6161663
Kyle Mills <kyle.mills@uoit.net>
2014-09-28 16:41:05 -0400

    huge_deletion_of_simulation_data_I_Have_elsewhere

--- BoronNitrideAluminum/01_structural_relaxation/plot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-grep pressure OUTCAR | awk '{print $4}' > pressure
-gnuplot -persist plot_pressure.gnuplot
--- BoronNitrideAluminum/09hp_SiatN/plot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-grep pressure OUTCAR | awk '{print $4}' > pressure
-grep temperature OUTCAR | awk '$7 ~ "^[0-9,\.][0-9,\.]*$" {print $7}' > temperature
-gnuplot -persist plot_pressure.gnuplot
-gnuplot -persist plot_temperature.gnuplot
