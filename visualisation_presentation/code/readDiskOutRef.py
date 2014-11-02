3b32b6765623cffeb0186038f72365dbafb55e4f
Kyle Mills <kyle.mills@uoit.net>
2014-11-02 14:10:50 -0500

    cleanup

--- visualisation_presentation/code/readDiskOutRef.py
+++
@@ -1,5 +0,0 @@
-from paraview.simple import *
-path = '/Users/razoumov/Dropbox/visualization/data/other/'
-reader = ExodusIIReader(FileName=path+'disk_out_ref.ex2')
-Show()
-Render()
--- visualisation_presentation/code/remote.py
+++
@@ -1,6 +0,0 @@
-from paraview.simple import *
-sphere = Sphere()
-print sphere.ThetaResolution
-sphere.ThetaResolution = 16
-Show()
-WriteImage('image.png')
