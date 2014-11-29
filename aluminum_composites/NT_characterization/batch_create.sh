8117ae7584fd84e2af365e91a49af4cb9ae4e800
Kyle Mills <kyle.mills@uoit.net>
2014-11-28 23:33:37 -0500

    nanotube characterization vasp run generation scripts

---
+++ aluminum_composites/NT_characterization/batch_create.sh
@@ -0,0 +1,22 @@
+sh generate_job.sh 6 0 Al N 1.890
+sh generate_job.sh 5 0 Al N 1.890
+sh generate_job.sh 10 0 Al N 1.890
+sh generate_job.sh 12 0 Al N 1.890
+
+sh generate_job.sh 6 6 Al N 1.890
+sh generate_job.sh 5 5 Al N 1.890
+sh generate_job.sh 10 10 Al N 1.890
+sh generate_job.sh 12 12 Al N 1.890
+
+
+#sh generate_job.sh 6 3 Al N 1.890
+#sh generate_job.sh 4 2 Al N 1.890
+#sh generate_job.sh 10 5 Al N 1.890
+#sh generate_job.sh 12 6 Al N 1.890
+
+
+
+
+
+
+#
