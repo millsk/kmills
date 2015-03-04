e634ff3f28269fff9072d05a56476653d61704fc
Kyle Mills <kyle.mills@uoit.net>
2015-03-04 00:18:29 -0500

    added NT analysis script

---
+++ aluminum_composites/Krasheninnikov_paper/AlNAl/1-perfect/3-sheet/vasp.s
@@ -0,0 +1,23 @@
+#!/bin/bash
+#PBS -l walltime=72:00:00
+#PBS -l pmem=2000mb
+#PBS -r n
+#PBS -l procs=112
+#PBS -M kyle.mills@uoit.net
+
+#PBS -N "sheet_AlAl111_rel"
+
+cd $PBS_O_WORKDIR
+echo -n "START " > datefile
+date >> datefile
+
+touch WAVECAR
+touch CHGCAR
+
+module purge
+module load intel-2011
+
+dir=`pwd`
+ndir="${dir}_1"
+
+mpiexec /global/software/VASP5/vasp-5.3.3/vasp > ./log 
--- aluminum_composites/NT_characterization/batch_create.sh
+++ aluminum_composites/NT_characterization/batch_create.sh
@@ -1,22 +1,51 @@
-sh generate_job.sh 6 0 Al N 1.890
-sh generate_job.sh 5 0 Al N 1.890
-sh generate_job.sh 10 0 Al N 1.890
-sh generate_job.sh 12 0 Al N 1.890
 
-sh generate_job.sh 6 6 Al N 1.890
-sh generate_job.sh 5 5 Al N 1.890
-sh generate_job.sh 10 10 Al N 1.890
-sh generate_job.sh 12 12 Al N 1.890
+#sh generate_job.sh n m element1 element2 bond_length workstation
+
+#BL:  1.446 (BNNT)
+#     1.446 (AlNT)
 
 
-#sh generate_job.sh 6 3 Al N 1.890
-#sh generate_job.sh 4 2 Al N 1.890
-#sh generate_job.sh 10 5 Al N 1.890
-#sh generate_job.sh 12 6 Al N 1.890
+# (n, n/2)
+
+sh generate_job.sh 2 1 B N 1.446 06
+sh generate_job.sh 4 2 B N 1.446 06
+sh generate_job.sh 6 3 B N 1.446 06
+sh generate_job.sh 8 4 B N 1.446 06
+
+sh generate_job.sh 10 5 B N 1.446 05
+sh generate_job.sh 12 6 B N 1.446 05
+sh generate_job.sh 14 7 B N 1.446 05
 
 
+# (n, n)
+
+sh generate_job.sh 2 2 B N 1.446 11
+sh generate_job.sh 3 3 B N 1.446 11
+sh generate_job.sh 4 4 B N 1.446 11
+sh generate_job.sh 5 5 B N 1.446 11
+sh generate_job.sh 6 6 B N 1.446 11
+sh generate_job.sh 7 7 B N 1.446 11
+sh generate_job.sh 8 8 B N 1.446 11
+
+sh generate_job.sh 9 9 B N 1.446 13
+sh generate_job.sh 10 10 B N 1.446 13
+sh generate_job.sh 11 11 B N 1.446 13
 
 
+# (n, 0)
+
+sh generate_job.sh 2 0 B N 1.446 10
+sh generate_job.sh 3 0 B N 1.446 10
+sh generate_job.sh 4 0 B N 1.446 10
+sh generate_job.sh 5 0 B N 1.446 10
+
+sh generate_job.sh 6 0 B N 1.446 09
+sh generate_job.sh 7 0 B N 1.446 09
+sh generate_job.sh 8 0 B N 1.446 09
+sh generate_job.sh 9 0 B N 1.446 09
+
+sh generate_job.sh 10 0 B N 1.446 07
+sh generate_job.sh 11 0 B N 1.446 07
+sh generate_job.sh 12 0 B N 1.446 07
 
 
-#
