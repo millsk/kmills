986d80a2144eaa4e490739f6ee5e2034a220b1de
Kyle Mills <kyle.mills@uoit.net>
2015-03-07 16:53:04 -0500

    nanotube analysis bash script updated

--- aluminum_composites/NT_characterization/analysis.sh
+++ aluminum_composites/NT_characterization/analysis.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo='echo -e '
+
+
+if [ "x$1" == "xheader" ]; then
+#Dump the header
+   $echo "SYSTEM\tNATOMS\tTOTEN\tE_PER_ATOM\tE_HOMO\tE_LUMO\tE_GAP\tDIAMETER"
+   exit 0
+fi
 
 
 function  NTdiameter() {
@@ -29,11 +37,30 @@ function  NTdiameter() {
 
 function HOMOLUMO () {
 
-pcregrep -M '2\.00000.*(\n)*.*2\.00000.*(\n)*.*0\.00000.*(\n)*.*0\.00000' OUTCAR | tail -4 | head -3 | tail -2 | awk '{print $1,"\t",$2,"\t",$3}'
+   pcregrep -M '2\.00000.*(\n)*.*2\.00000.*(\n)*.*0\.00000.*(\n)*.*0\.00000' OUTCAR | tail -4 | head -3 | tail -2 | awk '{print $2}'
 
 }
 
+function energy_per_atom() {
+        NATOMS=$(awk 'NR==7 {for(i=1;i<=NF;i++) t+=$i; print t; t=0}' POSCAR)
+        TOTEN=$(grep TOTEN OUTCAR | tail -n 1 | awk '{print $5 }')
+#        echo -e "TOTEN  \t\t NATOMS "
+#        echo -e "$energy \t $atoms "
+}
+
+
+SYSTEMNAME=$(echo "${PWD##*/}")
+
+diameter=$(NTdiameter)
+E_HOMO=$(HOMOLUMO | head -n 1)
+E_LUMO=$(HOMOLUMO | tail -n 1)
+energy_per_atom
+E_GAP=$(bc <<< "scale=6; $E_LUMO - $E_HOMO")
+E_per_atom=$(bc <<< "scale=6; $TOTEN / $NATOMS " )
+
+
+$echo "$SYSTEMNAME\t$NATOMS\t$TOTEN\t$E_per_atom\t$E_HOMO\t$E_LUMO\t$E_GAP\t$diameter"
 
 
 
-for d in $(ls -d *AlNNT*); do echo $d; cd $d; NTdiameter; HOMOLUMO; cd ..; echo -e "\n\n\n"; done
+#for d in $(ls -d *AlNNT*); do echo $d; cd $d; NTdiameter; HOMOLUMO; cd ..; echo -e "\n\n\n"; done
