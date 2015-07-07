517b1d87b4d9c71dbc9fbe1e8d021a14d4a5718b
Kyle Mills <kyle.mills@uoit.net>
2015-07-07 12:59:20 -0400

    hexcore allows hollow tubes

--- aluminum_composites/structure_generation/wurtzite_aln_nanorod/hexcore.py
+++ aluminum_composites/structure_generation/wurtzite_aln_nanorod/hexcore.py
@@ -7,10 +7,20 @@ import math
 import sys
 
 
-filename = "POSCAR_bulk"
+filename = "bulk.dat"
 
-size = int(sys.argv[1])  #distance from center to node of polygon
+#size = int(sys.argv[1])  #distance from center to node of polygon
 polygon_sides = 6  #hexagon
+
+
+#define outer/inner diameters.  They will be changed slightly 
+outer_diameter = 330.
+inner_diameter = 0.
+
+
+
+
+
 
 
 poscar = ase.io.vasp.read_vasp(filename)
@@ -24,44 +34,57 @@ a3 = np.array(cell[2])
 
 #The center of the cell is the half way point of each lattice vector, added together.
 center = (a1+a2)/2.
-print center
-
+print "Center of cell: ",center
 center = center[:2]
-print center
+
+#original list of atoms (all, before removal)
 o = np.array(poscar._get_positions())[:,:2]
-print o
 
+#Find the closest atom to the center.  This is the "hexagon size"
 closest_dist = 9999999999.
-
 for atom in o:
    if np.linalg.norm((atom-center)) < closest_dist:
       closest = atom
       closest_dist = np.linalg.norm((atom-center))
-
-
 print "the closest atom to the center is at ",closest,", a distance of ",closest_dist,"away."
-node = (closest - center) * 1.05 * closest_dist * size
 
-nodes = []
-
-
-theta = 2*pi / polygon_sides
-
-print "Hexagon vertices:"
-for n in range(polygon_sides):
-   rot_matr = np.matrix( [ [ math.cos(n*theta), -math.sin(n*theta)],[ math.sin(n*theta), math.cos(n*theta) ] ] )
-   new = np.array([node[0],node[1]]) * rot_matr + [center[0],center[1]]
-   print new
-   nodes = nodes + [[ new.item(0), new.item(1) ]]
-#   nodes = np.concatenate( nodes,  new[0]  ) 
-#nodes = np.append(nodes, rot_matr*node)
+size_outer = int((outer_diameter/2.0) / closest_dist)/math.sqrt(3)
+size_inner = int((inner_diameter/2.0) / closest_dist)/math.sqrt(3)
+print size_outer
 
 
+
+outer_node = (closest - center) * 1.05 * closest_dist * size_outer
+
+print "outer node is a distance away from center of: ",np.linalg.norm((outer_node-center))
+
+#inner_node = (closest - center) * 1.05 * closest_dist * int((inner_diameter/2.0)/closest_dist)
+
+inner_node = (closest - center) * 1.05 * closest_dist * size_inner
+
+print  "outer_node,inner_node:  ", outer_node,inner_node
+
+
+def build_polygon(tcenter,tnode,tpolygon_sides):
+  print "build_polygon() called with params: ", tcenter, tnode, tpolygon_sides, "."
+  #Build the polygon mask:
+  nodes = []
+  theta = 2*pi / tpolygon_sides
+  print "Hexagon vertices:"
+  for n in range(tpolygon_sides):  #for each vertex... 
+     rot_matr = np.matrix( [ [ math.cos(n*theta), -math.sin(n*theta)],[ math.sin(n*theta), math.cos(n*theta) ] ] )
+     new = np.array([tnode[0],tnode[1]]) * rot_matr + [tcenter[0],tcenter[1]]
+     print new
+     nodes = nodes + [[ new.item(0), new.item(1) ]]
+  return nodes
+
+
+
+
+#Function to determine if point lies inside polygon
 def point_in_poly(x,y,poly):
-
     n = len(poly)
     inside = False
-
     p1x,p1y = poly[0]
     for i in range(n+1):
         p2x,p2y = poly[i % n]
@@ -73,15 +96,16 @@ def point_in_poly(x,y,poly):
                     if p1x == p2x or x <= xints:
                         inside = not inside
         p1x,p1y = p2x,p2y
-
     return inside
 
 
 
+outside_border = build_polygon(center,outer_node,polygon_sides)
+inside_border = build_polygon(center,inner_node,polygon_sides)
 
+del poscar[ [ atom.index for atom in poscar if not point_in_poly(atom.position[0],atom.position[1],outside_border) ]]
+del poscar[ [ atom.index for atom in poscar if point_in_poly(atom.position[0],atom.position[1],inside_border) ]]
 
-
-del poscar[ [ atom.index for atom in poscar if not point_in_poly(atom.position[0],atom.position[1],nodes) ]]
 
 
 print len(poscar._get_positions())," atoms left"
@@ -93,7 +117,7 @@ print len(poscar._get_positions())," atoms left"
 #      #del poscar[i]
 
 
-ase.io.write('POSCAR_{0}'.format(size),poscar,format='vasp')
+ase.io.write('POSCAR_{0}'.format(outer_diameter),poscar,format='vasp')
 
 
 
